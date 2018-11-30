b167828168f428dfca956f9f56ff81adb04edc17
Dominik Menke <dom@digineo.de>
2018-11-30 16:50:42 +0100

    Prometheus: extend labels

--- app/prometheus.go
+++ app/prometheus.go
@@ -1,49 +1,54 @@
 package app
 
 import (
-	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // PrometheusExporter is a Prometheus metrics endpoint.
-type PrometheusExporter struct {
+type PrometheusExporter struct{}
+
+func init() {
+	prom := &PrometheusExporter{}
+	prometheus.MustRegister(prom)
 }
 
-var _ prometheus.Collector = (*PrometheusExporter)(nil) // type check
-
 var (
-	hostLabels      = []string{"host"}
-	descLastSuccess = prometheus.NewDesc(joinLabels("last_success"), "duration since last success in seconds", hostLabels, nil)
+	hostLabels       = []string{"host"}
+	descLastSuccess  = prometheus.NewDesc("zackup_last_success", "duration since last success in seconds", hostLabels, nil)
+	descLastDuration = prometheus.NewDesc("zackup_last_duration", "duration of last successful run in seconds", hostLabels, nil)
+	descSpaceTotal   = prometheus.NewDesc("zackup_space_used", "total space used for backups in bytes", hostLabels, nil)
+	descSpaceSnaps   = prometheus.NewDesc("zackup_space_used_by_snapshots", "space used by snapshots in bytes", hostLabels, nil)
+	descCompression  = prometheus.NewDesc("zackup_compression", "compression ratio", hostLabels, nil)
 )
 
 // Describe implements the prometheus.Collector interface
 func (e PrometheusExporter) Describe(c chan<- *prometheus.Desc) {
 	c <- descLastSuccess
+	c <- descLastDuration
+	c <- descSpaceTotal
+	c <- descSpaceSnaps
+	c <- descCompression
 }
 
 // Collect implements the prometheus.Collector interface
 func (e PrometheusExporter) Collect(c chan<- prometheus.Metric) {
 	now := time.Now()
 
-	for _, metrics := range state.export() {
-		labels := []string{metrics.Host}
+	for _, m := range state.export() {
+		labels := []string{m.Host}
 
-		since := float64(-1)
-		if succeededAt := metrics.SucceededAt; succeededAt != nil {
-			since = float64(now.Sub(*succeededAt) / time.Second)
+		since, dur := float64(-1), float64(-1)
+		if m.SucceededAt != nil {
+			since = float64(now.Sub(*m.SucceededAt) / time.Second)
+			dur = float64(m.SuccessDuration) / float64(time.Second)
 		}
+
 		c <- prometheus.MustNewConstMetric(descLastSuccess, prometheus.CounterValue, since, labels...)
+		c <- prometheus.MustNewConstMetric(descLastDuration, prometheus.GaugeValue, dur, labels...)
+		c <- prometheus.MustNewConstMetric(descSpaceTotal, prometheus.GaugeValue, float64(m.SpaceUsedTotal), labels...)
+		c <- prometheus.MustNewConstMetric(descSpaceSnaps, prometheus.GaugeValue, float64(m.SpaceUsedBySnapshots), labels...)
+		c <- prometheus.MustNewConstMetric(descCompression, prometheus.GaugeValue, m.CompressionFactor, labels...)
 	}
-}
-
-func joinLabels(parts ...string) string {
-	parts = append([]string{"zackup"}, parts...)
-	return strings.Join(parts, "_")
-}
-
-func init() {
-	prom := &PrometheusExporter{}
-	prometheus.MustRegister(prom)
 }
