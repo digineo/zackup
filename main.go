d757f64f498550e31e12e4c35bc7e9ae6dfdbe80
Dominik Menke <dom@digineo.de>
2018-12-03 16:37:55 +0100

    Debug: dump goroutines on SIGUSR1

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"math/rand"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 	"time"
 
 	"git.digineo.de/digineo/zackup/cmd"
 )
 
 func main() {
+	go handleSIGUSRx()
+
 	rand.Seed(time.Now().UnixNano())
 	cmd.Execute()
+}
+
+func handleSIGUSRx() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGUSR1)
+
+	for range sig {
+		pprof.Lookup("goroutine").WriteTo(os.Stderr, 2)
+	}
 }
