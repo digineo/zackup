dc8d5ac0617c72b20c9ed9c40f6e118f3264726d
Dominik Menke <dom@digineo.de>
2018-12-11 10:48:18 +0100

    zackup status: pretty-print structs
    - `-v`   prints job structs
    - `-vvv` prints config tree

--- cmd/status.go
+++ cmd/status.go
@@ -7,6 +7,7 @@ import (
 
 	"git.digineo.de/digineo/zackup/app"
 	humanize "github.com/dustin/go-humanize"
+	"github.com/k0kubun/pp"
 	"github.com/spf13/cobra"
 )
 
@@ -45,11 +46,17 @@ func colorize(s app.MetricStatus) string {
 	return s.String()
 }
 
-// statusCmd represents the status command
+// statusCmd represents the status command. Its Run func deals mostly
+// with output formatting.
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Prints a list of hosts and their backup status",
 	Run: func(cmd *cobra.Command, args []string) {
+		if verbosity > 2 {
+			pp.Println(tree)
+			return
+		}
+
 		exported := app.ExportState()
 
 		wantAll := len(args) == 0
@@ -77,32 +84,35 @@ var statusCmd = &cobra.Command{
 			s := host.Status()
 			fmt.Printf("%-[1]*s  status            %s\n", longest, host.Host, colorize(s))
 
-			if s == app.StatusPrimed {
-				// we don't know anything yet
-				continue
+			// we don't know anything yet if primed
+			if s != app.StatusPrimed {
+				if s == app.StatusUnknown || s == app.StatusRunning {
+					fmt.Printf("%s  started           %s\n", ws, statusTime(&host.StartedAt))
+				}
+				if s == app.StatusUnknown || s == app.StatusSuccess {
+					t := statusTime(host.SucceededAt)
+					d := statusDur(host.SuccessDuration)
+					fmt.Printf("%s  succeeded at      %s (took %s)\n", ws, t, d)
+				}
+				if s == app.StatusUnknown || s == app.StatusFailed {
+					t := statusTime(host.FailedAt)
+					d := statusDur(host.FailureDuration)
+					fmt.Printf("%s  failed at         %s (took %s)\n", ws, t, d)
+				}
+
+				fmt.Printf("%s  space used        %s (%s snapshots, %s dataset, %s children, %s refreservation)\n", ws,
+					humanize.Bytes(host.SpaceUsedTotal()),
+					humanize.Bytes(host.SpaceUsedBySnapshots),
+					humanize.Bytes(host.SpaceUsedByDataset),
+					humanize.Bytes(host.SpaceUsedByChildren),
+					humanize.Bytes(host.SpaceUsedByRefReservation))
+				fmt.Printf("%s  compression       %0.2fx\n", ws, host.CompressionFactor)
 			}
 
-			if s == app.StatusUnknown || s == app.StatusRunning {
-				fmt.Printf("%s  started           %s\n", ws, statusTime(&host.StartedAt))
+			if verbosity > 0 {
+				job := tree.Host(host.Host)
+				pp.Println(job)
 			}
-			if s == app.StatusUnknown || s == app.StatusSuccess {
-				t := statusTime(host.SucceededAt)
-				d := statusDur(host.SuccessDuration)
-				fmt.Printf("%s  succeeded at      %s (took %s)\n", ws, t, d)
-			}
-			if s == app.StatusUnknown || s == app.StatusFailed {
-				t := statusTime(host.FailedAt)
-				d := statusDur(host.FailureDuration)
-				fmt.Printf("%s  failed at         %s (took %s)\n", ws, t, d)
-			}
-
-			fmt.Printf("%s  space used        %s (%s snapshots, %s dataset, %s children, %s refreservation)\n", ws,
-				humanize.Bytes(host.SpaceUsedTotal()),
-				humanize.Bytes(host.SpaceUsedBySnapshots),
-				humanize.Bytes(host.SpaceUsedByDataset),
-				humanize.Bytes(host.SpaceUsedByChildren),
-				humanize.Bytes(host.SpaceUsedByRefReservation))
-			fmt.Printf("%s  compression       %0.2fx\n", ws, host.CompressionFactor)
 		}
 	},
 }
